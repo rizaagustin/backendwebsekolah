d3c7d5866c78d8889bb77d8a8ed47ea1cdda45e4
Riza Agustin <rizaagustin20@gmail.com>
2024-04-04 18:30:55 +0700

    login halaman

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,7 +13,7 @@
     <section class="vh-100">
       <div class="container-fluid">
         <div class="row align-items-center">
-          <div class="col-sm-6 text-black p-5">
+          <div class="col-md-6 text-black p-5">
               <h3 class="fw-normal mb-3 pb-3 text-center" style="letter-spacing: 1px;">Silahkan Login</h3>
               @foreach ($errors->all() as $error)
                 <p class="text-danger">
@@ -35,8 +35,8 @@
             </form>
           </div>
 
-          <div class="col-sm-6 px-0 d-none d-sm-block">
-              <img src="{{ asset('assets/dist/img/sekolah.jpg') }}" alt="Login image" class="w-100 vh-100" style="object-fit: cover; object-position: left;">
+          <div class="col-md-6 px-0 d-none d-sm-block">
+            <img src="{{ asset('assets/dist/img/sekolah.jpg') }}" srcset="{{ asset('assets/dist/img/sekolah.jpg') }} 480w, {{ asset('assets/dist/img/sekolah.jpg') }} 1024w" alt="Login image" class="w-100 vh-100" style="object-fit: cover; object-position: left;">
           </div>
         </div>
       </div>
